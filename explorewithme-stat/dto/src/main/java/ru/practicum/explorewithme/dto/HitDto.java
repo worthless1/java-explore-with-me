6551b2a3a0271265c158028996bbb5e16d5b3134
Dmitry Yerkin <imoodivance1@gmail.com>
2023-10-06 00:54:15 +0300

    Minor fixes

--- explorewithme-stat/dto/src/main/java/ru/practicum/explorewithme/dto/HitDto.java
+++ explorewithme-stat/dto/src/main/java/ru/practicum/explorewithme/dto/HitDto.java
@@ -3,6 +3,7 @@ package ru.practicum.explorewithme.dto;
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -12,12 +13,15 @@ public class HitDto {
     private Long id;
 
     @NotBlank(message = "app name cannot be empty")
+    @Size(max = 128)
     private String app;
 
     @NotBlank(message = "URI cannot be empty")
+    @Size(max = 128)
     private String uri;
 
     @NotBlank(message = "user's IP address cannot be empty")
+    @Size(max = 16)
     private String ip;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
